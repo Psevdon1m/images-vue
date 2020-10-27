151a3277243b50e03937e38ca0266ee8cbc6f459
Valentin <valentin@elephantslab.com>
2020-10-27 16:49:38 +0200

    add upload images func, image styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <AppHeader></AppHeader>
-        <router-view></router-view>
+        <div class="ui container">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -15,6 +17,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,35 @@
 <template>
-    <div>Image List</div>
+    <div>
+        <div class="image-container" v-if="isLoggedIn">
+            <img v-for= "image in allImages" :src="image.link" :key="image.id"/>
+        </div>
+        <h2 v-else>Login to get started.</h2>
+    </div>
+    
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex';
+
 export default {
-    name: 'ImageList'
+    name: 'ImageList',
+    methods: mapActions(['fetchImages']),
+    computed: mapGetters(['allImages', 'isLoggedIn']),
+    created() {
+        this.fetchImages();
+    }
+
 }
 </script>
+
+<style scoped>
+    .image-container{
+        column-count: 3;
+        column-gap: 0;
+    }
+
+    img{
+        max-width: 100%;
+        padding: 5px;
+    }
+</style>
